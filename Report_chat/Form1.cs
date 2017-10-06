98752628989628dd6c1f02eb43f0feb30b375e6f
subham samir <subham samir@UNISYSTER>
2017-10-07 02:11:33 +0530

    Add project files.

---
+++ Report_chat/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+
+namespace Report_chat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string source = @"Data Source = UNISYSTER\SQLEXPRESS; Initial Catalog = iimt; Integrated Security = True";
+                SqlConnection con = new SqlConnection(source);
+                con.Open();
+                MessageBox.Show("connected");
+                string sqlselectquery = "SELECT * FROM College_login WHERE ID = " + int.Parse(textBox1.Text);
+                SqlCommand cmd = new SqlCommand(sqlselectquery, con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    textBox2.Text = (dr["College_name"].ToString());
+                }
+                else
+                {
+
+                }
+                con.Close();
+            
+        }
+    }
+}
